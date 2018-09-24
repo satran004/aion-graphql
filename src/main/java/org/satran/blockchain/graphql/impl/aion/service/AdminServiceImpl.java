3bf7b99fbc92199539a6c56a68d5a7b65162ba9e
satran004 <tanay123>
2018-09-24 20:39:16 +0800

    Returns blocks in descending order. Recent blocks first.

--- src/main/java/org/satran/blockchain/graphql/impl/aion/service/AdminServiceImpl.java
+++ src/main/java/org/satran/blockchain/graphql/impl/aion/service/AdminServiceImpl.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -240,12 +241,15 @@ public class AdminServiceImpl implements AdminService {
                 logger.error("Error getting block details" + apiMsg.getErrString());
             }
 
-            List<BlockDetails> block = (List<BlockDetails>) apiMsg.getObject();
+            List<BlockDetails> blocks = (List<BlockDetails>) apiMsg.getObject();
 
             if (logger.isDebugEnabled())
-                logger.debug("Result: " + block.toString());
+                logger.debug("Result: " + blocks.toString());
 
-            return block.stream()
+            if(blocks != null)
+                Collections.reverse(blocks);
+
+            return blocks.stream()
                     .map(blockDetails -> ModelConverter.convert(blockDetails))
                     .collect(Collectors.toList());
 

a594dfa8daafda66b7c6a7a1548e0f68fe8ae89a
satran004 <satran004@gmail.com>
2018-10-15 09:48:13 +0000

    Gradle script change to include integration test package

---
+++ src/integration-test/java/org/satran/blockchain/graphql/it/TxnApiTest.java
@@ -0,0 +1,50 @@
+package org.satran.blockchain.graphql.it;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class TxnApiTest {
+
+//  @LocalServerPort
+//  private int port;
+//
+//  TestRestTemplate restTemplate = new TestRestTemplate();
+//
+//  HttpHeaders headers = new HttpHeaders();
+//
+//  @Value("${transactions.query}")
+//  private String transactionsQuery;
+//
+//  @Before
+//  public void setUp() {
+//    System.out.println(transactionsQuery);
+//  }
+//
+//  @Test
+//  public void testTransaction() throws Exception {
+//
+////    String query = "";
+////    HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+////
+////    ResponseEntity<String> response = restTemplate.exchange(
+////        createURLWithPort(),
+////        HttpMethod.POST, entity, String.class);
+////
+////    System.out.printf(response.getBody());
+//   // String actual = response.getHeaders().get(HttpHeaders.LOCATION).get(0);
+//
+//  }
+//
+//  private String createURLWithPort() {
+//    return "http://localhost:" + port + "/graphql";
+//  }
+
+  @Test
+  public void dummyTest() {
+
+  }
+}

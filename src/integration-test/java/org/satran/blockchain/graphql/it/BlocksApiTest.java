b74f1fc31b0f63066dbb96ecca86c2a870eb5c62
satran004 <satran004@gmail.com>
2018-10-25 22:12:44 +0800

    Add test for blockApi

--- src/integration-test/java/org/satran/blockchain/graphql/it/BlocksApiTest.java
+++ src/integration-test/java/org/satran/blockchain/graphql/it/BlocksApiTest.java
@@ -1,5 +1,69 @@
 package org.satran.blockchain.graphql.it;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.startsWith;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("it")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BlocksApiTest {
 
+    @LocalServerPort
+    private int port;
+
+    @Value("${blocks.query}")
+    private String blocksQuery;
+
+    TestRestTemplate restTemplate = new TestRestTemplate();
+
+    @Test
+    public void getBlocksTest() throws Exception {
+        System.out.println(blocksQuery);
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<String>(blocksQuery, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+            TestUtil.createURLWithPort(port),
+            HttpMethod.POST, entity, String.class);
+
+        System.out.printf(response.toString() );
+
+        JsonArray jsonArray = TestUtil.getJSONArray(response.getBody(), "blockApi", "blocks");
+
+        assertThat(jsonArray.size(), equalTo(5));
+        assertThat(jsonArray.get(0).getAsJsonObject().get("number").getAsLong(), is(not(equalTo(0))));
+        assertThat(jsonArray.get(0).getAsJsonObject().get("hash").getAsString(), is(notNullValue()));
+
+        assertThat(jsonArray.get(0).getAsJsonObject().get("nrgConsumed").getAsLong(), is(not(equalTo(0))));
+
+        assertThat(jsonArray.get(0).getAsJsonObject().getAsJsonArray("txDetails").size(), is(greaterThan(0)));
+
+        assertThat(jsonArray.get(0).getAsJsonObject().getAsJsonArray("txDetails")
+            .get(0).getAsJsonObject().get("timestamp").getAsLong(), is(not(equalTo(0))));
+
+        JsonObject txnObj = jsonArray.get(0).getAsJsonObject().getAsJsonArray("txDetails")
+            .get(0).getAsJsonObject();
+
+    }
 }
--- src/integration-test/java/org/satran/blockchain/graphql/it/TestUtil.java
+++ src/integration-test/java/org/satran/blockchain/graphql/it/TestUtil.java
@@ -1,5 +1,37 @@
 package org.satran.blockchain.graphql.it;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TestUtil {
 
+    public static String createURLWithPort(int port) {
+        return "http://localhost:" + port  + "/graphql";
+    }
+
+    public static JsonArray getJSONArray(String json, String api, String method) {
+        JsonParser jsonParser = new JsonParser();
+        JsonElement jsonElement = jsonParser.parse(json);
+
+        JsonArray jsonArray = jsonElement.getAsJsonObject().get("data").getAsJsonObject()
+            .get(api).getAsJsonObject()
+            .get(method).getAsJsonArray();
+
+        return jsonArray;
+    }
+
+    public static JsonObject getJsonObject(String json, String api, String method) {
+        JsonParser jsonParser = new JsonParser();
+        JsonElement jsonElement = jsonParser.parse(json);
+
+        JsonObject jsonObject = jsonElement.getAsJsonObject().get("data").getAsJsonObject()
+            .get(api).getAsJsonObject()
+            .get(method).getAsJsonObject();
+
+        return jsonObject;
+    }
 }
